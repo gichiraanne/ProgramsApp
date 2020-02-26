5738c0ea150f55562e9766d3ae4192d6d4930a8e
Annie <gichiraanne@gmail.com>
2020-02-26 13:50:47 +0300

    Adjust app module to use new file structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,21 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { HttpClientModule, HttpClient } from "@angular/common/http";
-import { StoreModule } from "@ngrx/store";
-import { EffectsModule } from "@ngrx/effects";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { DatePipe } from "@angular/common";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DatePipe } from '@angular/common';
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { ProgramsComponent } from "./programs/programs-list/programs.component";
-import { ProgramsReducer } from "./programs/programs.reducers";
-import { ProgramEffects } from "./programs/programs.effects";
-import { ActivitiesComponent } from "./activities/activities-list/activities.component";
-import { ActivityReducer } from "./activities/activities.reducers";
-import { ActivityEffects } from "./activities/activities.effects";
-import { ActivityNewComponent } from "./activities/activity-new/activity-new.component";
-import { ActivityEditComponent } from "./activities/activity-edit/activity-edit.component";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProgramsComponent } from './programs/programs-list/programs.component';
+import { ProgramsReducer } from './programs/state/programs.reducers';
+import { ProgramEffects } from './programs/state/programs.effects';
+import { ActivitiesComponent } from './activities/activities-list/activities.component';
+import { ActivityReducer } from './activities/state/activities.reducers';
+import { ActivityEffects } from './activities/state/activities.effects';
+import { ActivityNewComponent } from './activities/activity-new/activity-new.component';
+import { ActivityEditComponent } from './activities/activity-edit/activity-edit.component';
 
 @NgModule({
   declarations: [
