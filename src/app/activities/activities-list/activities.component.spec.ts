2cb787c8b0ac5d30b7f6a8f932ec94a3d0a07ad2
Annie <gichiraanne@gmail.com>
2020-02-26 07:44:31 +0300

    Updates to activities list

--- src/app/activities/activities-list/activities.component.spec.ts
+++ src/app/activities/activities-list/activities.component.spec.ts
@@ -1,27 +1,45 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { ActivitiesComponent } from "./activities.component";
+import { NO_ERRORS_SCHEMA } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { provideMockStore, MockStore } from "@ngrx/store/testing";
+import { Store } from "@ngrx/store";
+import { of } from "rxjs";
 
-import { ActivitiesComponent } from './activities.component';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
-
-describe('ActivitiesComponent', () => {
+describe("ActivitiesComponent", () => {
   let component: ActivitiesComponent;
   let fixture: ComponentFixture<ActivitiesComponent>;
+  let mockActivatedRoute;
+  let mockStore: MockStore<[]>;
+  mockActivatedRoute = {
+    snapshot: {
+      params: {
+        get: () => "578"
+      }
+    }
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ActivitiesComponent ],
+      declarations: [ActivitiesComponent],
+      providers: [
+        provideMockStore(),
+        { provide: ActivatedRoute, useValue: mockActivatedRoute }
+      ],
       schemas: [NO_ERRORS_SCHEMA]
-    })
-    .compileComponents();
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ActivitiesComponent);
+    mockStore = TestBed.get(Store);
     component = fixture.componentInstance;
+    Object.defineProperty(component, "activities", { writable: true });
+    component.activities = of({ activities: [] });
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
--- src/app/activities/activities-list/activities.component.ts
+++ src/app/activities/activities-list/activities.component.ts
@@ -6,7 +6,6 @@ import { ActivityState } from "../activities.reducers";
 import { map } from "rxjs/operators";
 import { Subscription, Observable } from "rxjs";
 import { Activity } from "src/app/models/activity.model";
-import { FormControl, Validators, FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: "app-activities",
@@ -20,19 +19,15 @@ export class ActivitiesComponent implements OnInit {
   activityList: Activity[];
   activityToEdit: Activity;
   showEditModal: boolean;
-  editActivityForm: FormGroup;
-
 
   constructor(
     private activatedRoute: ActivatedRoute,
-    private store: Store<{ activities: ActivityState }>,
-    private formBuilder: FormBuilder
+    private store: Store<{ activities: ActivityState }>
   ) {
     this.activities = this.store.select("activities");
   }
 
   ngOnInit() {
-    console.log(this.store);
     this.programId = this.activatedRoute.snapshot.params["id"];
     this.ToDoSubscription = this.activities
       .pipe(
@@ -59,14 +54,10 @@ export class ActivitiesComponent implements OnInit {
    * filters actityList to get actity to edit
    * @param activityId The id of the activity to edit
    */
-  showActivityModal(activityId){
-
-    this.activityToEdit = this.activityList.filter(activity => activity.id == activityId)[0];
-
-    // console.log(this.activityToEdit.name)
-
-
+  showActivityModal(activityId) {
+    this.activityToEdit = this.activityList.filter(
+      activity => activity.id == activityId
+    )[0];
     this.showEditModal = true;
   }
-
 }
